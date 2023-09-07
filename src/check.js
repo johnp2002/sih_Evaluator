fd597447030c0083757bb18756f97ff0eca67cc1
JOHN P <patchalajohn@gmail.com>
2023-09-07 14:47:51 +0530

    final commit

--- src/check.js
+++ src/check.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useCookies } from 'react-cookie';
-
+import sha256 from 'sha256';
+import Evaulator from './Evaulator';
+import App from './App';
 
 function check() {
     const [cookies, setCookie] = useCookies(['name']);
+    const adm = 'dd1fa9ccd5604d8d4f75a035ea570570e5242f40df502d774eda7b7f9af3e1d1';
+    const [auth,setAuth] = React.useState(true)
    const [user,setUser] = React.useState('')
   const handleChange=(e)=>{
     setUser(e.target.value)
@@ -12,8 +16,17 @@ function check() {
 
   return (
     <div>
-        <input onChange={(e)=>{handleChange(e)}} value={user} />
-        <h1>{user}</h1>
+        {(auth)?
+
+        <>
+            {(sha256(user) === adm) || <input type='password' onChange={(e)=>{handleChange(e)}} value={user} />}
+            {/* <h1>{user}</h1> */}
+            {(sha256(user) === adm) && <Evaulator/>}
+            {(sha256(user) === adm) || <App/>}
+        </>
+
+        : <></>
+        }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,19 +5,15 @@ import App from './App';
 import Evaulator from './Evaulator';
 import check from './check';
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/' exact Component={App} />
-        <Route path='/evaluator' exact Component={Evaulator} />
-        {/* <App /> */}
+        <Route path='/' exact Component={check} />
+        {/* <Route path='/evaluator' exact Component={check} /> */}
       </Routes>
     </Router>
+    
   </React.StrictMode>
 );
